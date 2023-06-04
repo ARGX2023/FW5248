51f80411d4b2ccab71ad3928f9c7d26d152fc15f
ARGX2023 <128996443+ARGX2023@users.noreply.github.com>
2023-06-04 22:25:51 +0800

    Revert "Revert "re-back + ferre to carry""
    This reverts commit d1ec53bc211beba92b3b524253cfa696d366f0c4.

---
+++ bothsides.pas
@@ -0,0 +1,41 @@
+program bothsides;
+uses Math;
+var 
+integer, X,Y : text;
+data : LongInt;
+
+begin
+  //Solve for X data 
+  write( 'X=pi16-2t/-2');
+  Writeln();
+  write( 'X=pi16-2t/-2');
+  Writeln();
+  //Divide 2x - pi16 by -2 data
+  write( 't= -X + pi16/2');
+  Writeln();
+  //Dividing by -2 data blockwrite the multiplied in -2 data
+  write( 't= 2x -pi16/-2');
+  Writeln();
+  //Divide -2 data blockwrite the multiply data and divide both sides by -2 data
+  write( '-2t/-2 = 2x -pi16/-2');
+  Writeln();
+  //Multiply bothsides of data for the equation by 2 data
+  write( '-2t = 2x -pi16');
+  writeln();
+  //Subtract -pi16/2 data for  both side of each data 
+  write( '-t=X&-pi16/2');
+  writeln();
+  Write( '-t=X&-pi16/2');
+  writeln();
+  //Swap sides of each data for var terms from left and right on each posible file equation terms of data 
+  write( 'pi16/2 -t=X');
+  writeln();
+  
+  //Write data To screen 
+  Writeln(data);
+  read;
+  
+  
+  readln;
+  
+end.{special Æ(-&pi16/2,-&pi16/2)}
